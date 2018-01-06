5baa9fd1293696530d450fb51b18872320bb67f7
yaymo <stevenjamesonhill@gmail.com>
2018-01-06 16:25:01 -0500

    fix auth route

--- server/services/passport.js
+++ server/services/passport.js
@@ -19,7 +19,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
+    callbackURL: '/auth/google/callback'
   }, 
 	(accessToken, refreshToken, profile, done) => {
 		User.findOne({ googleId: profile.id })
