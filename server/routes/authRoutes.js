268c7366f7eb1b1d518f814c8ee93c06e3f3c4db
yaymo <stevenjamesonhill@gmail.com>
2018-01-06 15:38:17 -0500

    auth

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,4 +8,13 @@ module.exports = app => {
 
 	app.get('/auth/google/callback', passport.authenticate('google'));
 
+	app.get('/api/currentUser', (req, res) => {
+		//res.send(req.user);
+		res.send(req.session);
+	});
+
+	app.get('/api/logout', (req, res) => {
+		req.logout();
+		res.send(req.user);
+	});
 };
