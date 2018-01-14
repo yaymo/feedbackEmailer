b1fbb30bbddf9cccdcc0978e9f884950032f4fd6
yaymo <stevenjamesonhill@gmail.com>
2018-01-14 11:55:38 -0500

    redeploy after environ var change

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component {
     render() {
         return (
             <StripeCheckout 
+                name="Emailer"
+                description="$5 for 5 email credits"
                 amount={500}
-                token={token => console.log(token)}
+                token={token => this.props.handleToken(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-                />
+                >
+                <button className="btn">
+                    Add Credits
+                </button>
+            </StripeCheckout>
         );
     }
 }
-export default Payments;
+export default connect(null, actions)(Payments);
