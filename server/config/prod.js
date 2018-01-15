83a12de6d527aa196550a8b0300552e6a02223e3
yaymo <stevenjamesonhill@gmail.com>
2018-01-15 15:48:14 -0500

    hardcode uri

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
+    mongoURI: 'mongodb://yaymo:Monday1234@ds245337.mlab.com:45337/udemyemailer-prod',
     cookieKey: process.env.COOKIE_KEY,
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY
