228ca3420c9f63b6d06b73d669d816c0d12d9785
yaymo <stevenjamesonhill@gmail.com>
2018-01-18 14:11:07 -0500

    dummy text

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Landing = () => {
     return (
         <div style={{textAlign: 'center'}}>
-            <h1>Test componenet</h1>
+            <h1>Test component</h1>
             Test test test
         </div>
     )
