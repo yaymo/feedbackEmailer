f1f710cd89de2617dee72406535135b6f66f588d
yaymo <stevenjamesonhill@gmail.com>
2018-02-05 20:57:32 -0500

    refactor header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -2,48 +2,44 @@ import React, {Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Payments from './Payments';
+import './styles/Header.css';
 
 
-const titleStyles = {
-    marginLeft: '20px',
-}
-
-const navStyles = {
-    marginBottom: '25px'
-}
-
-class Header extends Component {
-    renderContent() {
+class Header extends Component {    
+    renderLogin() {
         switch(this.props.auth) {
             case null:
                 return;
             case false:
                 return (
-                    <li><a href="/auth/google">Login With Google</a></li>
+                    <li key="1"><a href="/auth/google">Login With Google</a></li>
                 );
             default: 
                 return [
-                    <li key="1"><Payments /></li>,
+                    <li key="1"><a href="/dashboard">Dashboard</a></li>,
                     <li key="2" style={{ margin: '0 10px' }}>
-                        Credits: {this.props.auth.credits}
+                        <a href="/credits">
+                        Credits
+                        </a>
                     </li>,
-                    <li key="3"><a href="/api/logout">Logout</a></li>
+                    <li key="1"><a href="/api/logout">Logout</a></li>
                 ];
         }
     }
     render() {
         return (
-            <nav style={navStyles}>
-                <div className="nav-wrapper">
-                    <Link to={this.props.auth ? '/surveys' : '/'}
-                        className="left brand-logo" href="/"
-                        style={titleStyles}>
-                        Survey.Me
-                    </Link>
-                    <ul className="right">
-                        {this.renderContent()}
-                    </ul>
-                </div>
+                <nav>
+                    <div className="nav-wrapper">
+                        <ul className="left">
+                            <Link to={this.props.auth ? '/surveys' : '/'}
+                                className="brand-logo nav-item" href="/">
+                                Survey.Me
+                            </Link>
+                        </ul>
+                            <ul className="right">
+                                {this.renderLogin()}
+                            </ul>
+                    </div>
             </nav>
         );
     }
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -10,7 +10,6 @@ import reducers from './reducers';
 import axios from 'axios';
 window.axios = axios;
 
-
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
