92ef170fe142791a9e31b5096c347339ec41bd30
yaymo <stevenjamesonhill@gmail.com>
2018-05-03 21:23:32 -0400

    Added inline editable field component

---
+++ server/client/src/components/utils/EditableField/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class EditableField extends Component {
+    static propTypes = {
+        text: PropTypes.string
+    }
+
+    static defaultProps = {
+        text: ''
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: this.props.text || '',
+            isEditing: false
+        }
+    }
+
+    handleToggle = () => {
+        this.setState({ isEditing: !this.state.isEditing });
+    }
+
+    handleChange = () => {
+        this.setState({ text: this.textInput.value });
+    }
+
+    render() {
+        const { text, isEditing } = this.state;
+
+        if(isEditing) {
+            return (
+                <input value={ text } 
+                ref={(input) => { this.textInput = input }}
+                onChange={ this.handleChange }
+                onBlur={ this.handleToggle } />
+            )
+        }
+        return (
+            <h3 onClick={ this.handleToggle }>
+                {text}
+            </h3>
+        )
+    }
+}
+
+export default EditableField;
