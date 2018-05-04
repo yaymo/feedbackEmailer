d485d74372c8adc40c1624c128d2d02b84ac8b70
yaymo <stevenjamesonhill@gmail.com>
2018-05-04 19:10:37 -0400

    Made editableField a sfc

--- server/client/src/components/utils/EditableField/index.js
+++ server/client/src/components/utils/EditableField/index.js
@@ -1,46 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class EditableField extends Component {
-    static propTypes = {
-        text: PropTypes.string
-    }
-
-    static defaultProps = {
-        text: ''
-    }
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: this.props.text,
-            isEditing: false
-        }
-    }
-
-    handleToggle = () => {
-        this.setState({ isEditing: !this.state.isEditing });
-    }
-
-    handleChange = () => {
-        this.setState({ text: this.textInput.value });
-    }
-
-    render() {
-        const { text, isEditing } = this.state;
-
-        if(isEditing) {
+const EditableField = (props) => {
+        if(props.isEditing) {
             return (
-                <input value={ text } type='text'
-                onChange={ this.handleChange }
-                onBlur={ this.handleToggle } autoFocus/>
+                <input value={ props.text } type='text'
+                onChange={ props.handleChange }
+                onBlur={ props.handleToggle } autoFocus/>
             )
         }
         return (
-            <h3 onClick={ this.handleToggle } style={{ cursor: 'pointer'}}>
-                {text}
+            <h3 onClick={ props.handleToggle } style={{ cursor: 'pointer'}}>
+                {props.text}
             </h3>
         )
     }
-}
 
 export default EditableField;
