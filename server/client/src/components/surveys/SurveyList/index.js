73d1ffad2d66b6c24a65198d5e4569e21ae4e13b
yaymo <stevenjamesonhill@gmail.com>
2018-06-09 12:01:12 -0400

    Add placeholder to title filter add button for filtering by date

--- server/client/src/components/surveys/SurveyList/index.js
+++ server/client/src/components/surveys/SurveyList/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Button } from 'react-bootstrap';
 import Card from '../../Card/';
 import LoadingIndicator from '../../Loading/';
 import { deleteSurvey } from '../../../actions/';
-import { connect } from 'react-redux';
+
 
 export class SurveyList extends React.Component  {
 
@@ -46,7 +48,12 @@ export class SurveyList extends React.Component  {
     });
     return (
       <div className="container">
-        <input onChange={ (e) => this.props.handleChange(e.target.value) } style={{width: '200px', height: '40px', marginTop: '30px'}}/>
+        <input onChange={ (e) => this.props.handleChange(e.target.value) }
+          placeholder="Enter Survey Title"
+          style={{width: '250px', height: '50px', marginTop: '30px', marginRight: '30px'}}/>
+        <Button bsStyle="primary" bsSize="large" onClick={ () => console.log('clicking')}>
+          Oldest To Newest
+        </Button>
         { this.props.isLoading && <LoadingIndicator /> }
         <div className="row">
           {surveys}
