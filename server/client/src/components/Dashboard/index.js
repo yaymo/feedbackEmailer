df07aecc60833c3553eaad0f33f460682836272c
yaymo <stevenjamesonhill@gmail.com>
2018-06-19 19:19:25 -0400

    use spaces. Get rid of useless constructor

--- server/client/src/components/Dashboard/index.js
+++ server/client/src/components/Dashboard/index.js
@@ -8,61 +8,55 @@ import './Dashboard.css';
 
 export class Dashboard extends Component {
 
-    constructor(props) {
-        super(props);
+  state = { filterText: ''};
 
-        this.state = {
-            filterText: ''
-        }
-    }
+  static propTypes = {
+    auth: PropTypes.shape({
+      credits: PropTypes.number
+    }),
+    isLoading: PropTypes.bool,
+    surveys: PropTypes.array,
+    fetchSurveys: PropTypes.func
+  }
 
-    static propTypes = {
-        auth: PropTypes.shape({
-            credits: PropTypes.number
-        }),
-        isLoading: PropTypes.bool,
-        surveys: PropTypes.array,
-        fetchSurveys: PropTypes.func
-    }
+  static defaultProps = {
+    auth: {
+      credits: 0
+    },
+    isLoading: false,
+    surveys: [],
+    fetchSurveys: () => {}
+  }
 
-    static defaultProps = {
-        auth: {
-            credits: 0
-        },
-        isLoading: false,
-        surveys: [],
-        fetchSurveys: () => {}
-    }
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
 
-    componentDidMount() {
-        this.props.fetchSurveys();
-    }
+  handleChange = (filterText) => {
+    this.setState({filterText});
+  }
 
-    handleChange = (filterText) => {
-        this.setState({filterText});
-    }
-
-    render() {
-        const { isLoading } = this.props;
-        const surveys = this.props.surveys.filter(survey => survey.title.includes(this.state.filterText.trim().toLowerCase() || ''));
-        return  (
-            <div>
-                <SurveyList surveys={surveys} isLoading={isLoading}
-                    handleChange={filterText => this.handleChange( filterText )} />
-                <div className="fixed-action-btn">
-                    {this.props.auth && this.props.auth.credits ?
-                        <Link to="/surveys/new" className="btn btn-danger btn-lg plus">
-                            +
-                        </Link>
-                        :
-                        <button className="btn btn-secondary btn-lg plus zero">
-                            +
-                        </button>
-                    }
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { isLoading, auth } = this.props;
+    const surveys = this.props.surveys.filter(survey => survey.title.includes(this.state.filterText.trim().toLowerCase() || ''));
+    return  (
+      <div>
+        <SurveyList surveys={surveys} isLoading={isLoading}
+          handleChange={filterText => this.handleChange( filterText )} />
+        <div className="fixed-action-btn">
+          {auth && auth.credits ?
+            <Link to="/surveys/new" className="btn btn-danger btn-lg plus">
+              +
+            </Link>
+            :
+            <button className="btn btn-secondary btn-lg plus zero">
+              +
+            </button>
+          }
+        </div>
+      </div>
+    );
+  }
 }
 function mapStateToProps({ auth, surveys: { surveys, isLoading }}) {
     return {
