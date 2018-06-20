f3ae7b319f981453d59c7122ac5fe0ba88762983
yaymo <stevenjamesonhill@gmail.com>
2018-06-19 21:24:55 -0400

    Make filters more responsive

--- server/client/src/components/surveys/SurveyFiltersList/index.js
+++ server/client/src/components/surveys/SurveyFiltersList/index.js
@@ -1,20 +1,23 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import './surveyFiltersList.css';
 
 const SurveyFiltersList = ({ handleChange, handleSortAsc, handleSortDesc }) => {
   return (
-    <Fragment>
+    <div className="row">
+    <div className="col-lg-4 col-md-12 col-sm-12">
       <input onChange={(e) => handleChange(e.target.value)}
-        placeholder="Enter survey title" id="filterInput" />
+        placeholder="Enter survey title" id="filterInput" /></div>
+      <div className="col-lg-4 col-md-12 col-sm-12">
       <Button bsStyle="primary" bsSize="large" onClick={handleSortAsc} className="sort-btn" id="btn-asc">
         Oldest To Newest
-      </Button>
+      </Button></div>
+      <div className="col-lg-4 col-md-12 col-sm-12">
       <Button bsStyle="info" bsSize="large" onClick={handleSortDesc} className="sort-btn" id="btn-desc">
         Newest To Oldest
-      </Button>
-    </Fragment>
+      </Button></div>
+    </div>
   )
 }
 SurveyFiltersList.propTypes = {
