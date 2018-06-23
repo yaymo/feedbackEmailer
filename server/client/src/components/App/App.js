d408cbadba1bd30dffe21f8377e8df35a517a083
yaymo <stevenjamesonhill@gmail.com>
2018-06-23 18:49:11 -0400

    use spaces

--- server/client/src/components/App/App.js
+++ server/client/src/components/App/App.js
@@ -14,34 +14,34 @@ import './App.css';
 
 class App extends Component {
 
-    static propTypes = {
-        fetchUser: PropTypes.func
-    }
+  static propTypes = {
+    fetchUser: PropTypes.func
+  }
 
-    static defaultProps = {
-        fetchUser: () => {}
-    }
-    componentDidMount() {
-        this.props.fetchUser();
-    }
-    render() {
-        return (
-            <div>
-                <BrowserRouter>
-                    <div>
-                        <Header />
-                        <div className="container">
-                            <Route exact path='/' component={Landing} />
-                            <Route exact path='/surveys' component={Dashboard} />
-                            <Route path='/surveys/new' component={SurveyNew} />
-                            <Route path='/credits' component={Credits} />
-                            <Route path='/contacts' component={Contacts} />
-                        </div>
-                    </div>
-                </BrowserRouter>
+  static defaultProps = {
+    fetchUser: () => {}
+  }
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path='/' component={Landing} />
+            <div className="container">
+              <Route exact path='/surveys' component={Dashboard} />
+              <Route path='/surveys/new' component={SurveyNew} />
+              <Route path='/credits' component={Credits} />
+              <Route path='/contacts' component={Contacts} />
             </div>
-        );
-    }
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default connect(null, actions)(App);
