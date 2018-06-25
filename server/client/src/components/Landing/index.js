100cf2a8f92e06219d62ce8848e9b9aee35baed6
yaymo <stevenjamesonhill@gmail.com>
2018-06-25 19:04:41 -0400

    added typewriter effect to header text

--- server/client/src/components/Landing/index.js
+++ server/client/src/components/Landing/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
+import './landing.css';
+import Footer from '../Footer';
 
 const Landing = () => {
-  const containerStyle = {
-    textAlign: 'center',
-    width: '100%',
-    height: '100%',
-    margin: '0px auto 20px auto',
-    background: 'white',
-    paddingTop: '40px'
-  }
   return (
     <React.Fragment>
-      <div style={containerStyle}>
-        <h2 style={{marginBottom: '50px', fontFamily: 'Roboto', fontSize: '40px', color: '#555'}}>Welcome to Loop.io!</h2>
-        <p style={{fontFamily: '25px', marginTop: '50px'}}>The app that always keeps you up to date with product feedback</p>
+      <div className="landing-container">
+        <div className="">
+          <h2 className="landing-header">Welcome to Loop.io!</h2>
+          <p className="landing-description">The app that always keeps you up to date with product feedback</p>
+        </div>
+        <div className="features">Check out some of our features <a href="/features">HERE</a></div>
       </div>
+      <Footer />
     </React.Fragment>
   )
 }
