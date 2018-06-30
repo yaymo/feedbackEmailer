7384e00d91e40a9a718f4ac4b78f4156fc36ce91
yaymo <stevenjamesonhill@gmail.com>
2018-06-30 15:47:10 -0400

    use spaces

--- server/client/src/components/surveys/SurveyFiltersList/index.js
+++ server/client/src/components/surveys/SurveyFiltersList/index.js
@@ -6,17 +6,18 @@ import './surveyFiltersList.css';
 const SurveyFiltersList = ({ handleChange, handleSortAsc, handleSortDesc }) => {
   return (
     <div className="row">
-    <div className="col-lg-4 col-md-12 col-sm-12">
-      <input onChange={(e) => handleChange(e.target.value)}
-        placeholder="Enter survey title" id="filterInput" /></div>
       <div className="col-lg-4 col-md-12 col-sm-12">
-      <Button bsStyle="primary" bsSize="large" onClick={handleSortAsc} className="sort-btn" id="btn-asc">
-        Oldest To Newest
-      </Button></div>
-      <div className="col-lg-4 col-md-12 col-sm-12">
-      <Button bsStyle="info" bsSize="large" onClick={handleSortDesc} className="sort-btn" id="btn-desc">
-        Newest To Oldest
-      </Button></div>
+        <input onChange={(e) => handleChange(e.target.value)}
+          placeholder="Enter survey title" id="filterInput" /></div>
+        <div className="col-lg-4 col-md-12 col-sm-12">
+        <Button bsStyle="primary" bsSize="large" onClick={handleSortAsc} className="sort-btn" id="btn-asc">
+          Oldest To Newest
+        </Button></div>
+        <div className="col-lg-4 col-md-12 col-sm-12">
+        <Button bsStyle="info" bsSize="large" onClick={handleSortDesc} className="sort-btn" id="btn-desc">
+          Newest To Oldest
+        </Button>
+      </div>
     </div>
   )
 }
--- server/client/src/components/surveys/SurveyFormReview/index.js
+++ server/client/src/components/surveys/SurveyFormReview/index.js
@@ -9,41 +9,41 @@ import * as actions from '../../../actions/';
 import './SurveyFormReview.css';
 
 export const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
-    const reviewFields = _.map(formFields, ({ name, label }) => {
-        return (
-            <div key={name} className="review-fields">
-                <label className="form-label"> {label} </label>
-                <div className="form-value">
-                    {formValues[name]}
-                </div>
-            </div>
-        );
-    })
-    return (      
-        <div className="survey-review">
-            <h5 className="header">Please confirm your entries</h5>
-            {reviewFields}
-            <Button bsStyle="warning" bsSize="large" onClick={onCancel}>
-                Back
-            </Button>
-            <Button onClick={() => submitSurvey(formValues, history)} bsStyle="success" bsSize="large" className="pull-right">
-                Send Survey
-            </Button>
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name} className="review-fields">
+        <label className="form-label"> {label} </label>
+        <div className="form-value">
+          {formValues[name]}
         </div>
+      </div>
     );
+  })
+  return (
+    <div className="survey-review">
+      <h5 className="header">Please confirm your entries</h5>
+      {reviewFields}
+      <Button bsStyle="warning" bsSize="large" onClick={onCancel}>
+        Back
+      </Button>
+      <Button onClick={() => submitSurvey(formValues, history)} bsStyle="success" bsSize="large" className="pull-right">
+        Send Survey
+      </Button>
+    </div>
+  );
 };
 
 SurveyFormReview.propTypes = {
-    onCancel: PropTypes.func,
-    submitSurvey: PropTypes.func,
-    history: PropTypes.object,
-    formValues:PropTypes.object
+  onCancel: PropTypes.func,
+  submitSurvey: PropTypes.func,
+  history: PropTypes.object,
+  formValues:PropTypes.object
 }
 
 function mapStateToProps(state) {
-    return {
-        formValues: state.form.surveyForm.values
-    }
+  return {
+    formValues: state.form.surveyForm.values
+  }
 }
 
 export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
